d568b76595c0f086250a2fda7b1683b62905bfee
martinjup <97602564+martinjup@users.noreply.github.com>
2023-02-27 16:02:47 -0400

    Login funcionalidad
    falta css

--- MentalIfy/src/views/login/login.jsx
+++ MentalIfy/src/views/login/login.jsx
@@ -1,39 +1,51 @@
 import React, { useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
 import { auth } from "../../../src/firebase/firebaseConfig";
+import { useNavigate } from "react-router-dom";
+import { useCallback } from 'react'
+import './login.css'
 
-export function Login ()  {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const logIn = (e) => {
-      e.preventDefault();
-      signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          console.log(userCredential);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
-  
-    return (
-      <div className="sign-in-container">
-        <form onSubmit={logIn}>
-          <h1>Login</h1>
-          <input
-            type="email"
-            placeholder="Correo"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          ></input>
-          <input
-            type="password"
-            placeholder="Contraseña"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          ></input>
-          <button type="submit">Login</button>
-        </form>
+
+export const Login = () => {
+  const navigate = useNavigate()
+  const handleSubmit = useCallback(async e => {
+    e.preventDefault()
+
+    const { email, password } = e.target.elements
+    
+    try {
+      await signInWithEmailAndPassword(auth, email.value, password.value)
+      navigate('/')
+      alert('Login exitoso')
+    } catch (e) {
+      alert('Error en autenticacion, intente de nuevo')
+    }
+  }, [])
+
+  return (
+    <><div>
+      <div id='top'>.</div>
+      <div className='headboard'>
+        <h3>Login</h3>
       </div>
-    );
-  };
+      <div className = 'frame'>
+      <form id="form" onSubmit={handleSubmit}>
+        <div className='container'>
+          <input name="email" placeholder="correo" type="email" />
+        </div>
+        <div className='container'>
+          <input name="password" placeholder="contrase" type="password" />
+        </div >
+        <div className='container'>
+          <button type="submit" className='boton'>Login</button>
+        </div>
+      </form>
+      </div>
+      <div className='container'>
+      <button className='boton'> Google </button>
+      </div>
+      </div>
+    </>
+    
+  )
+}
