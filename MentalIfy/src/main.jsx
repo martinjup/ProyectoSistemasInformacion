0ab9b57464064215fb81d23da394b0c2a1a99b85
Abrahan1820 <104142023+Abrahan1820@users.noreply.github.com>
2023-03-08 14:07:03 -0400

    add userview not ready
    ...

--- MentalIfy/src/main.jsx
+++ MentalIfy/src/main.jsx
@@ -5,13 +5,15 @@ import {Home} from './views/home/home'
 import {Register} from './views/register/register'
 import {RegisterDoctor} from './views/register/registerdoctor'
 import {Login} from './views/login/login'
+import {UserView} from './views/userView/userView'
 // import {Nav} from './components/Navbar/Navbar'
 import './index.css'
 import {
   HOME_URL,
   LOGIN_URL,
   REGISTER_URL,
-  REGISTER_DOCTOR_URL
+  REGISTER_DOCTOR_URL,
+  USER_VIEW_URL
 
 } from './constants/urls'
 
@@ -25,6 +27,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Route path={LOGIN_URL} element={<Login />}/>
       <Route path={REGISTER_URL} element={<Register />}/>
       <Route path={REGISTER_DOCTOR_URL} element={<RegisterDoctor />}/>
+      <Route path={USER_VIEW_URL} element={<UserView />}/>
 
       
     </Routes>
